40b8484dfb97bafe574b4210d091f3cb9bfc6f30
Daniel Litt <daniel_litt@comcast.net>
2020-08-08 17:34:26 -0400

    Updates

---
+++ precache-manifest.e7b28a672a67fcd4cad3dbb74defb4e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8831f3d27b31efe71773a5d2404ce2ed",
+    "url": "/RobsNotWrong/index.html"
+  },
+  {
+    "revision": "898083e5be6778708269",
+    "url": "/RobsNotWrong/static/css/main.1bcfdc76.chunk.css"
+  },
+  {
+    "revision": "fc9e829013ad4b148945",
+    "url": "/RobsNotWrong/static/js/2.41b387c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RobsNotWrong/static/js/2.41b387c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "898083e5be6778708269",
+    "url": "/RobsNotWrong/static/js/main.7d98b81c.chunk.js"
+  },
+  {
+    "revision": "2913c1466a78f30be56b",
+    "url": "/RobsNotWrong/static/js/runtime-main.d834f035.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RobsNotWrong/static/media/logo.5d5d9eef.svg"
+  }
+]);
